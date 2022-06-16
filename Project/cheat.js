38ca1e769a91b16b933f40da378d18ac11580a34
SmashedAdam <73280731+SmashedAdam@users.noreply.github.com>
2022-06-16 10:09:10 +0800

    modified

--- Project/cheat.js
+++ Project/cheat.js
@@ -2,8 +2,10 @@ function getCode() {
     code = document.getElementById("cheatCode").value;
     if (cheatCode = "12345678") {
         localStorage.setItem('cheatCode', 'yes');
+        window.alert("Cheat activated")
     }
-    else { localStorage.setItem('cheatCode', 'no'); }
+    else { localStorage.setItem('cheatCode', 'no');
+window.alert("Not correct") }
 
 }
 //localStorage
